58b77a4d8b90a4c43723fe4fbd29ed6b2a9e7d07
Brian Clymer <crdnilfan@gmail.com>
2017-12-29 11:28:47 -0600

    Rendering the list of terms, list view is selectable.

---
+++ src/components/common/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View, Platform } from 'react-native';
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: Platform.OS === 'ios' ? 15 : 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
+
+export { Header };
